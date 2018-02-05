40c4ec028104adc866fd055b50e76becf1253327
Mhee <mhee@githb.com>
2018-02-05 22:09:13 +0700

    Version 5.2.2018
    Version 5.2.2018

---
+++ SmartPM/SmartPM/Views/EditProfile.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartPM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditAccount : ContentPage
+    {
+        public EditAccount()
+        {
+            InitializeComponent();
+        }
+
+        public async void changepic(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
